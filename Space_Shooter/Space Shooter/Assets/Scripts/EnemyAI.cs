82dc89d5f9588c2bb14070eaa0ec38dcd91a7074
Marius Žakevičius <marius.zakevicius@alchemetrics.co.uk>
2020-07-22 21:59:04 +0300

    Space shooter / Dungeon Escape initial

---
+++ Space_Shooter/Space Shooter/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public int enemyHealth = 1;
+
+    [SerializeField] private float _speed = 3.0f;
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private GameObject _explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * _speed);
+
+        if (transform.position.y < -6)
+        {
+            float randomXPosition = Random.Range(-8, 8);
+            transform.position = new Vector3(randomXPosition, 6.0f, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.tag)
+        {
+            case "Player":
+                Player p = other.GetComponent<Player>();
+                if (p) p.getDamage();
+                Spawn();
+                Die();
+                break;
+            case "Laser":
+                Spawn();
+                Destroy(other.gameObject);
+                Die();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Spawn()
+    {
+        float randomXPosition = Random.Range(-8, 8);
+        Instantiate(_enemy, new Vector3(randomXPosition, 6.0f, 0), Quaternion.identity);
+    }
+
+    private void Die()
+    {
+        Instantiate(_explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Space_Shooter/Space Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool canTripleShot = false;
+    public bool isSpeedBoosted = false;
+    public bool isShieldActive = false;
+    public int playerHealth = 3;
+
+    [SerializeField] private GameObject _laser;
+    [SerializeField] private GameObject _tripleShot;
+    [SerializeField] private GameObject _explosion;
+    [SerializeField] private GameObject _shield;
+    [SerializeField] private float _initialSpeed = 5.0f;
+    [SerializeField] private float _speedBoost = 2.0f;
+    [SerializeField] private float _fireRate = 0.25f;
+    [SerializeField] private float _powerupTimer = 5.0f;
+
+    private float _nextFire = 0.0f;
+    private Vector3 _laserPos = new Vector3(0, 1f, 0);
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        Debug.Log(Camera.main.WorldToScreenPoint(transform.position));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {        
+        if (Time.time > _nextFire)
+        {
+            if (canTripleShot)
+            {
+                Instantiate(_tripleShot, transform.position, Quaternion.identity);
+            } 
+            else
+            {
+                Instantiate(_laser, transform.position + _laserPos, Quaternion.identity);
+            }
+
+            _nextFire = Time.time + _fireRate;
+        }
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float speed = _initialSpeed;
+
+        if (isSpeedBoosted)
+        {
+            speed = _initialSpeed * _speedBoost;
+        }
+
+
+        // transform.Translate(new Vector3(1f, 0, 0) * Time.deltaTime * speed * horizontalInput);
+        // transform.Translate(new Vector3(0, 1f, 0) * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+    }
+
+    public void getDamage()
+    {
+        if (isShieldActive)
+        {
+            isShieldActive = false;
+            _shield.SetActive(false);
+            return;
+        } 
+        
+        --playerHealth;
+
+        if (playerHealth < 1)
+        {
+            Instantiate(_explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    public void TripleShotPowerupOn()
+    {
+        canTripleShot = true;
+        StartCoroutine(TripleShotTimerRoutine());
+    }
+
+    public IEnumerator TripleShotTimerRoutine()
+    {
+        yield return new WaitForSeconds(_powerupTimer);
+        canTripleShot = false;
+    }
+
+    public void SpeedPowerupOn()
+    {
+        isSpeedBoosted = true;
+        StartCoroutine(SpeedPowerupTimer());
+    }
+
+    public IEnumerator SpeedPowerupTimer()
+    {
+        yield return new WaitForSeconds(_powerupTimer);
+        isSpeedBoosted = false;
+    }
+
+    public void ShieldPowerUp()
+    {
+        isShieldActive = true;
+        _shield.SetActive(true);
+    }
+}
